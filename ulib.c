1faa624046452c96cee0d645600d29694f7caaad
recurze <vrahul24999@gmail.com>
2019-03-01 22:01:40 +0530

    Add support for negatives in atoi

--- ulib.c
+++ ulib.c
@@ -84,12 +84,17 @@ stat(const char *n, struct stat *st)
 int
 atoi(const char *s)
 {
-  int n;
+  int n, neg = 1;
+
+  if (s[0] == '-') {
+      ++s;
+      neg = -1;
+  }
 
   n = 0;
   while('0' <= *s && *s <= '9')
     n = n*10 + *s++ - '0';
-  return n;
+  return neg*n;
 }
 
 void*
